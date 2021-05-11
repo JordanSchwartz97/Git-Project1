4f6f9b28c59806b43b901b78009da716c07ee2fc
unknown <schwartzj1297@gmail.com>
2021-05-10 18:00:48 -0700

    Introduced random variable generators from the 4 inital array lists

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ let destinations = ["New York","Hawaii","Italy", "Australia"];
 let resturants = ["Mc donalds", "Joe's Italian Resturant", "Big John's seafood joint", "Chinese Dragon"];
 let modeOfTravel = ["Car", "Train", "Bike", "Plane"];
 let entertainment = ["Movies","Bowling","Sightseeing","Hiking"];
+
+function destinationGenerator(){
+    let randomDestination = destinations[Math.floor(Math.random() * destinations.length)];
+    return randomDestination;
+}
+let randomDestination = destinationGenerator();
+
+function resturantGenerator(){
+    let randomResturant = resturants[Math.floor(Math.random() * resturants.length)];
+    return randomResturant;
+}
+let randomResturant = resturantGenerator();
+
+function modeOfTravelGenerator(){
+    let randomModeOfTravel = modeOfTravel[Math.floor(Math.random() * modeOfTravel.length)];
+    return randomModeOfTravel;
+}
+let randomModeOfTravel = modeOfTravelGenerator();
+
+function entertainmentGenerator(){
+    let randomEntertainment = entertainment[Math.floor(Math.random() * entertainment.length)];
+    console.log(randomEntertainment);
+    return randomEntertainment;
+}
+let randomEntertainment = entertainmentGenerator();
