e12ccdb554f0e8b4ceb9e5cdd1c1ea9b8cd86f31
unknown <schwartzj1297@gmail.com>
2021-05-10 17:47:18 -0700

    Add initial array values for randomVacationGenerator established

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 "use strict";
 console.log('Hello World');
+
+let destinations = ["New York","Hawaii","Italy", "Australia"];
+let resturants = ["Mc donalds", "Joe's Italian Resturant", "Big John's seafood joint", "Chinese Dragon"];
