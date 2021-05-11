48e0cb53aec13473765acaa31173526d9fef4868
unknown <schwartzj1297@gmail.com>
2021-05-10 17:49:00 -0700

    Forgot to add remainder of array values for randomVacationGenerator

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log('Hello World');
 
 let destinations = ["New York","Hawaii","Italy", "Australia"];
 let resturants = ["Mc donalds", "Joe's Italian Resturant", "Big John's seafood joint", "Chinese Dragon"];
+let modeOfTravel = ["Car", "Train", "Bike", "Plane"];
+let entertainment = ["Movies","Bowling","Sightseeing","Hiking"];
